773c75d53404887208ccc3d802e0040a4bd106e0
praveen-raipuria <praveenraipuria@gmail.com>
2024-10-05 22:31:35 +0530

    all files

---
+++ src/oct02102024/ATBStudent.java
@@ -0,0 +1,23 @@
+package oct02102024;
+
+public class ATBStudent {
+
+    // C - ATBStudent
+    //  A - Attribute | data members | instance variables | fields | member variables
+    long phone;
+    String email;
+    int age;
+    int batch8x;
+    String name;
+    boolean isMale;
+    String address;
+    boolean isfeedPaid;
+    String githublink;
+    String sid;
+
+    //  B - Behaviour | methods | member functions
+    void study(){} // NRNA
+    void eat(){}
+    void walk(){}
+}
+
